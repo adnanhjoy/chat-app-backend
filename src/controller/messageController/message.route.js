a49b7173e164f423e1545150eb6d61662f4565f3
unknown <engr.adnanhossain@gmail.com>
2024-10-11 12:57:08 +0600

    [implement message route]

--- src/controller/messageController/message.route.js
+++ src/controller/messageController/message.route.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
+const { authMiddleware } = require('../../middleware/authMiddleware ');
 const messageController = require('../messageController/message.controller')
 
-router.post('/send', messageController.sendMessage)
-router.post('/receive', messageController.getMessages)
+router.post('/send', authMiddleware, messageController.sendMessage)
+router.get('/receive/:receiverId', authMiddleware, messageController.getMessages)
 
 module.exports = { messageRouter } = router
